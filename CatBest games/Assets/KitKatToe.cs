abd7924072ca6e1d87a0bb4ae9091f3fa594f68e
hihinkoland <kamil.slimak.sk@gmail.com>
2024-03-26 12:07:08 +0100

    Added KitKatToe controller script

---
+++ CatBest games/Assets/KitKatToe.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitKatToe : MonoBehaviour
+{
+    public GameObject TL;
+    public GameObject TC;
+    public GameObject TR;
+    public GameObject ML;
+    public GameObject MC;
+    public GameObject MR;
+    public GameObject BL;
+    public GameObject BC;
+    public GameObject BR;
+
+    public Dictionary<fieldPos, GameObject> fields = new Dictionary<fieldPos, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum fieldPos
+{
+    TopLeft,
+    TopCenter,
+    TopRight,
+    MiddleLeft,
+    MiddleCenter,
+    MiddleRight,
+    BottomLeft,
+    BottomCenter,
+    BottomRight
+}
